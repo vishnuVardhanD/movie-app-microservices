bbfee4843a54aa2bd9a2834dafea232e4ca404cc
D Vishnu Vardhan <vishnucse514@gmail.com>
2020-09-13 23:17:50 -0500

    Problems with Hystrix Proxy
    Hystrix can't see the fallback created for the private methods from the main method called from external service

--- movie-catalog-service/src/main/java/com/vish/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/vish/moviecatalogservice/resource/MovieCatalogResource.java
@@ -24,17 +24,42 @@ public class MovieCatalogResource {
 	RestTemplate restTemplate;
 	
 	@RequestMapping("/{movieId}")
-	@HystrixCommand(fallbackMethod = "getFallbackCatalog")
+	//@HystrixCommand(fallbackMethod = "getFallbackCatalog")
 	public CatalogItem getCatalogItem(@PathVariable("movieId") String movieId) {
 		
-		Movie movieInfo = restTemplate.getForObject("http://movie-info-service/movies/" + movieId, Movie.class);
-        Rating rating = restTemplate.getForObject("http://movie-rating-service/ratings/" + movieId, Rating.class);
+		Movie movieInfo = getMovieInfo(movieId);
+        Rating rating = getRating(movieId);
        
 		return new CatalogItem(movieInfo.getId(), movieInfo.getName(), rating.getRating());
     }
-	
-	public CatalogItem getFallbackCatalog(@PathVariable("movieId") String movieId) {
-		return new CatalogItem(movieId, "No Movie", "No Rating");
+
+	@HystrixCommand(fallbackMethod = "getFallbackRating")
+	private Rating getRating(String movieId) {
+		Rating rating = restTemplate.getForObject("http://movie-rating-service/ratings/" + movieId, Rating.class);
+		return rating;
 	}
+
+	@HystrixCommand(fallbackMethod = "getFallbackMovie")
+	private Movie getMovieInfo(String movieId) {
+		Movie movieInfo = restTemplate.getForObject("http://movie-info-service/movies/" + movieId, Movie.class);
+		return movieInfo;
+	}
+	
+	public CatalogItem getFallbackMovie(@PathVariable("movieId") String movieId) {
+		Movie movieInfo = new Movie();
+		movieInfo.setId(movieId);
+		movieInfo.setName("No Movie found");
+		movieInfo.setDesc("Movie Info service down");
+		return new CatalogItem(movieId, movieInfo.getName(), movieInfo.getDesc());
+	}
+	
+	public CatalogItem getFallbackRating(@PathVariable("movieId") String movieId) {
+		 Rating rating = new Rating();
+		 rating.setMovieId(movieId);
+		 rating.setRating("Rating service down");
+		return new CatalogItem(movieId, "No Movie found", rating.getRating());
+	}
+	
+	
 
 }
